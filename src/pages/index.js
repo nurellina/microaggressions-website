945132d3663cf975eb2cef2271bab89c015f0704
nurellina <ern246@nyu.edu>
2021-01-01 19:27:34 +0100

    Firebase github config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import styled from "styled-components"
-import CreatePhrase from "../components/create-phrase"
-import ShowPhrases from "../components/show-phrases"
 import HomePage from "../components/home-page/home-page"
 
 const IndexPage = () => (
