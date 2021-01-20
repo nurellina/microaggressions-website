bc8f88841176655c9126a0ecc3b31a988f2031eb
“zainab-tariq” <zainabtariq12@gmail.com>
2021-01-20 12:51:19 +0100

    reaction section wip

--- src/components/home-page/reaction.js
+++ src/components/home-page/reaction.js
@@ -7,10 +7,72 @@ const Reaction = () => {
   return (
     <div>
       {" "}
-      <article>
+      <article className="reaction">
         <div className="container">
-          <h1>How can people who are affected by microagressions react?</h1>
-          {/* your code here */}
+          <h1 className="clr-black">
+              {" "}
+              How can people who are affected by microagressions react?{" "}
+          </h1>
+            <p>
+            {" "}
+              If you find yourself in a situation in which you are confronted by microaggressions,
+              such like a difficult dialogue:
+            </p>
+            <p className="ff-accent allign-hcenter">
+            {" "}
+              First, think about whether or not it is worth continuing to talk to that person
+            </p>
+            <div className= "shape1">
+            <p className="clr-orange shape1Txt">
+              {" "}
+                Is this somebody who you think would actually have the capacity to hear what you have to say?
+              </p>
+              
+            </div>
+            <div className="split-r">
+              <div className="column-r">
+                <div className="shape2">
+                  <p className="clr-orange shape2Txt">
+                  {" "}
+                  no.
+                </p>
+                </div>
+              </div>  
+              <div className="column-r">
+                <div className="shape2">
+                  <p className="clr-orange shape2Txt">
+                    {" "}
+                    yes.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="split-r">
+              <div className="column-r">
+                <p>
+                {" "}
+                <br />A lot of times people get into arguments with people they don't need to 
+                  necessarily be emotionally invested in because they don't have that sort of 
+                  relationship.
+                </p>
+              </div>
+              <div className="column-r">
+                <p>
+                {" "}
+                <br />If you are close and if you do have a relationship it might be important
+                  just to say,<br />
+                  'Look I feel like we're both getting really emotionally charged right now. I don't feel like 
+                  I'm able to hear what you are saying. I don't feel that you're able to hear what I'm saying.
+                  <br />So maybe we need to table this and talk another time.'
+                </p>
+              </div>
+            </div>
+            <p className="ff-accent allign-hcenter">
+              {" "}
+              Or maybe offer to give them something they could read -
+              that could be more helpful or effective than a conversation that might just turn into 
+              yelling and hostality.
+            </p>
         </div>
       </article>
     </div>
