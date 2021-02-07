eaf3d3594aaa58bc6aa40730e35631177d0e097c
Ellina Nurmukhametova <ern246@nyu.edu>
2021-02-08 00:18:48 +0100

    Run prettier

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import "./layout.css"
@@ -25,7 +25,8 @@ const Header = ({ siteTitle, setMenuState, menuState }) => {
           {/* <div className="menu"> */}
           <div
             onClick={() => setMenuState(!menuState)}
-            className="hamburger-menu">
+            className="hamburger-menu"
+          >
             <span></span>
             <span></span>
             <span></span>
